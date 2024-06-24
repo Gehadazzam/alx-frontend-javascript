cfd1986d5b5b1dfcc0526bc95e539e3a65dad770
gehad <gehadgamalazzam@gmail.com>
2024-06-24 12:45:06 +0300

    Update task 0

---
+++ 0x00-ES6_basic/eslint.config.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    ignores: ['node_modules'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    rules: {
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      // Add your custom rules here
+    },
+  },
+];
+
