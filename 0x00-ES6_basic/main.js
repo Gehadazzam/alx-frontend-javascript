1bdc6e316e4d9cabcd5963c0827022f9428d731e
gehad <gehadgamalazzam@gmail.com>
2024-06-24 14:23:32 +0300

    Task 4 Count parameters

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,5 +1,5 @@
-import getSumOfHoods from './3-default-parameter.js';
 
-console.log(getSumOfHoods(34));
-console.log(getSumOfHoods(34, 3));
-console.log(getSumOfHoods(34, 3, 4));
+import returnHowManyArguments from './4-rest-parameter.js';
+
+console.log(returnHowManyArguments("one"));
+console.log(returnHowManyArguments("one", "two", 3, "4th"));
