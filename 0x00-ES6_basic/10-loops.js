0a475b999a7ddc0690a8d83a4ba2cc64c336a357
gehad <gehadgamalazzam@gmail.com>
2024-06-25 01:57:30 +0300

    Task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const appendedArray = array;
+  for (const val of array) {
+    const idx = array.indexOf(val);
+    appendedArray[idx] = appendString + val;
+  }
+  return array;
+}
