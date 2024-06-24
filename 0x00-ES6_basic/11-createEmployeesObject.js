e198e370a9e3c7918b896b9c90f65a9de272640c
gehad <gehadgamalazzam@gmail.com>
2024-06-25 02:03:45 +0300

    Task 11 make an obj

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,6 @@
+export default function createEmployeesObject(departmentName, employees) {
+    const departmentNameObj = {
+        [departmentName]: [...employees]
+    }; 
+    return departmentNameObj;
+}
