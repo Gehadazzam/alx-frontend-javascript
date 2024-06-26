0ecb3ad7ab33bfebceeddec9a72340c0e029d260
Gehadazzam <gehadgamalazzam@gmail.com>
2024-06-26 23:21:24 +0200

    Task 2 got response

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,16 @@
+function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+      console.log('Got a response from the API');
+    } else {
+      reject(new Error('The fake API is not working currently'));
+      console.log('Got a response from the API');
+    }
+  });
+}
+
+export default handleResponseFromAPI;
