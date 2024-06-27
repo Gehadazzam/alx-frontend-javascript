1b7fb0fe7ee1436ac5872718480312824322e04d
Gehadazzam <gehadgamalazzam@gmail.com>
2024-06-27 15:25:21 +0300

    Task 3 getter and setter and method

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+class Currency {
+  //  Constructor attributes:
+//   code(String)
+//   name(String)
+// Each attribute must be stored in an “underscore” attribute version(ex: name is stored in _name)
+  constructor(code, name) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // Implement a getter and setter for each attribute.
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  // Implement a method named displayFullCurrency that will return the attributes
+  // in the following format name(code).
+  displayFullCurrency() {
+    return `${this._name}(${this._code})`;
+  }
+}
+export default Currency;
