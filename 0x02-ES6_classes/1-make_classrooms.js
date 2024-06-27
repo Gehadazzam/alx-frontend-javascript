b6c9f3977a38a75094abc144f4978591b0bc4d55
Gehadazzam <gehadgamalazzam@gmail.com>
2024-06-27 15:03:36 +0300

    Task 1 initalize rooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  // It should return an array of 3 ClassRoom objects with the sizes 19, 20, and 34 (in this order).
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
+
+export default initializeRooms;
