dbc7f49f7cdccc81b9876020ef0c2e715491ec0c
Gehadazzam <gehadgamalazzam@gmail.com>
2024-07-02 21:45:52 +0300

    Task 6 set

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,7 @@
+// Create a function named setFromArray that returns a Set from an array.
+function setFromArray(array) {
+  // It accepts an argument(Array, of any kind of element).
+  return new Set(array);
+}
+
+export default setFromArray;
